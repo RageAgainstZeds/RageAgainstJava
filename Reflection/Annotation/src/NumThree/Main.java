c25cc9f5a8d8d07d4fe739971f95fdb2dc9f59d3
RageAgainst <uuvulaulu@gmail.com>
2017-12-11 23:07:37 +0200

    weird commit

--- Reflection/Annotation/src/NumThree/Main.java
+++ Reflection/Annotation/src/NumThree/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchFieldException, ClassNotFoundException {
         UnderDogClass under = new UnderDogClass(14, "Hail!", 19.39, "Text Me!");
         String str = Serilialization.serialize(under);
-        //System.out.println("Serilialized --> " + "\n" + str);
+        System.out.println("Serilialized --> " + "\n" + str);
 
         under = Serilialization.deserialize(str, UnderDogClass.class);
         System.out.println("Desirialization --> " + under.toString());
